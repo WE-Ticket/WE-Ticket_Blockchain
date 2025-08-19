1aa3bd8a47f5bf289fbb1b17da8411d826295b23
bbandm <hjerachel@gmail.com>
2025-08-19 15:45:59 +0900

    [FEAT] 옴니원 체인 연결

--- hardhat.config.js
+++ hardhat.config.js
@@ -9,7 +9,7 @@ module.exports = {
       url: "http://127.0.0.1:8545"
     },
     sepolia: {
-      url: process.env.SEPOLIA_URL,
+      url: process.env.CHAIN_URL,
       chainId: 11155111,
       accounts: process.env.PRIVATE_KEY && process.env.PRIVATE_KEY.length === 66 ? [process.env.PRIVATE_KEY] : [],
       timeout: 60000,
